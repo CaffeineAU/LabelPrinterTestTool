fa2ab1db48d10dcdd6217ab6947ead8f511d867c
Derek aprell <derektaprell@gmail.com>
2017-01-24 10:01:39 +1100

    Added 2 other projects

---
+++ SENOR-LIB/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SENOR_LIB
+{
+    public class Class1
+    {
+    }
+}
