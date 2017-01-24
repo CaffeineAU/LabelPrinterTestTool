1d3a385ee7a22c546580d9f1dfffb52da542ce1d
Derek aprell <derektaprell@gmail.com>
2017-01-24 15:16:04 +1100

    Added test for jira API

--- JIRA-Printer/MainWindow.xaml.cs
+++ JIRA-Printer/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Http;
+using System.Net;
+using System.IO;
 
 namespace JIRA_Printer
 {
@@ -20,9 +23,50 @@ namespace JIRA_Printer
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+
+            var webRequest = WebRequest.Create("http://jirapd.corp.resmed.org/rest/api/2/search?jql=assignee%20in%20(dtaprell)");
+
+            #region
+            string authorization = "Basic " + Base64Encode("username:password");
+            #endregion
+
+            webRequest.Headers.Add("Authorization", authorization);
+
+            /*using (Stream newStream = webRequest.GetRequestStream())
+            {
+                newStream.Write(ToBytes(requestPayload), 0, requestPayload.Length);
+                newStream.Flush();
+            }*/
+
+            var response = (HttpWebResponse)webRequest.GetResponse();
+
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                if (responseStream != null)
+                {
+
+                    //responseStream.CopyTo(memoryStream);
+                    String responseString = new StreamReader(responseStream).ReadToEnd();
+
+                    Clipboard.SetText(responseString);
+                    //return new JavaScriptSerializer().Deserialize<SupportedVoices>(responseString);
+                }
+                
+            }
+
+            
         }
     }
 }
