a8565ae28c33e33c0998b811815039d6a3425472
Liam O'Hagan <caffeineau@gmail.com>
2017-01-31 09:41:03 +1100

    Further support for adding / removing statuses

---
+++ JIRA-Printer/CustomCommands.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using JIRA_Printer;
+
+namespace JIRA_Printer.Commands
+{
+    public static class CustomCommands
+    {
+        public static readonly RoutedUICommand AddStatusCommand = new RoutedUICommand
+        (
+                "AddStatusCommand",
+                "AddStatusCommand",
+                typeof(CustomCommands),
+                new InputGestureCollection()
+                {
+                    new KeyGesture(Key.A, ModifierKeys.Alt)
+                }
+        );
+
+        public static readonly RoutedUICommand RemoveStatusCommand = new RoutedUICommand
+        (
+                "RemoveStatusCommand",
+                "RemoveStatusCommand",
+                typeof(CustomCommands),
+                new InputGestureCollection()
+                {
+                    new KeyGesture(Key.R, ModifierKeys.Alt)
+                }
+        );
+
+    }
+}
